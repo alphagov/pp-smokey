347811da9b21a8a5ccd820c1a15328c1100e7d1a
Matt Cottingham <matt.cottingham@digital.cabinet-office.gov.uk>
2014-11-12 14:34:44 +0000

    Process tabbed modules and all GroupBy params

--- flatten-validator/main.go
+++ flatten-validator/main.go
@@ -23,13 +23,14 @@ type DashboardConfigSummary struct {
 
 type DashboardConfig struct {
 	DashboardConfigSummary
-	// TODO(mattrco): Handle module-type: tab and decode all tabs.
 	Modules []Module
 }
 
 type Module struct {
 	Slug       string
 	Datasource DataSource `json:"data-source"`
+	ModuleType string     `json:"module-type"`
+	Tabs       []Module   `json:"tabs"`
 }
 
 type DataSource struct {
@@ -41,10 +42,10 @@ type DataSource struct {
 type QueryParameters struct {
 	SortBy  string   `json:"sort_by"`
 	Collect []string `json:"collect"`
-	// TODO(mattrco): GroupBy may be an array of strings or a string.
-	GroupBy  string   `json:"group_by"`
-	FilterBy []string `json:"filter_by"`
-	Limit    int      `json:"limit"`
+	// GroupBy may be an array of strings or a string.
+	GroupBy  interface{} `json:"group_by"`
+	FilterBy []string    `json:"filter_by"`
+	Limit    int         `json:"limit"`
 }
 
 func FetchDashboardConfigs(URL string) (DashboardConfigs, error) {
@@ -84,22 +85,40 @@ func FetchDashboardConfig(URL string) (DashboardConfig, error) {
 func ConstructModuleURL(module Module) (string, error) {
 	var err error
 
+	// TODO(mrc): use net/url to create the URL.
 	baseURL := fmt.Sprintf(
-		"https://www.performance.service.gov.uk/data/%s/%s?",
+		"https://www.staging.performance.service.gov.uk/data/%s/%s?",
 		module.Datasource.DataGroup,
 		module.Datasource.DataType,
 	)
 	moduleParams := module.Datasource.QueryParams
 	params := url.Values{}
 	if moduleParams.SortBy != "" {
-		params.Add("sort_by", module.Datasource.QueryParams.SortBy)
+		params.Add("sort_by", moduleParams.SortBy)
 	}
 	for _, collectParam := range moduleParams.Collect {
 		params.Add("collect", collectParam)
 	}
-	if moduleParams.GroupBy != "" {
-		params.Add("group_by", module.Datasource.QueryParams.GroupBy)
+
+	// Since GroupBy may be a string or a string array, switch on the type.
+	switch moduleParams.GroupBy.(type) {
+	case string:
+		groupBy := moduleParams.GroupBy.(string)
+		if groupBy != "" {
+			params.Add("group_by", groupBy)
+		}
+	case []interface{}:
+		groupBy := moduleParams.GroupBy.([]interface{})
+		for _, param := range groupBy {
+			stringParam, ok := param.(string)
+			if ok {
+				params.Add("group_by", stringParam)
+			} else {
+				return "", fmt.Errorf("Couldn't stringify param: %v", param)
+			}
+		}
 	}
+
 	for _, filter := range moduleParams.FilterBy {
 		params.Add("filter_by", filter)
 	}
@@ -110,12 +129,41 @@ func ConstructModuleURL(module Module) (string, error) {
 		err = errors.New("Empty query string encoded for module")
 	}
 
-	moduleURL := baseURL + params.Encode()
+	moduleURL := baseURL
+	if len(params) > 0 {
+		moduleURL = moduleURL + params.Encode()
+	}
 	return moduleURL, err
 }
 
+// Returns an array of modules, including any tabbed modules, from the dashboard config.
+func ListDashboardModules(dash DashboardConfig) []Module {
+	var modules []Module
+	for _, module := range dash.Modules {
+		if module.ModuleType == "tab" {
+			for _, tab := range module.Tabs {
+				modules = append(modules, tab)
+			}
+		} else {
+			modules = append(modules, module)
+		}
+	}
+	return modules
+}
+
+func validateResponse(moduleURL string) error {
+	resp, err := http.Get(moduleURL)
+	defer resp.Body.Close()
+	if err != nil {
+		return fmt.Errorf("Error fetching module: %s (%s)", err.Error(), moduleURL)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Got status %d for module %s", resp.StatusCode, moduleURL)
+	}
+	return nil
+}
+
 func main() {
-	baseURL := "https://stagecraft.production.performance.service.gov.uk/public/dashboards"
 	dashConfs, err := FetchDashboardConfigs(baseURL)
 	if err != nil {
 		log.Fatal(err.Error())
